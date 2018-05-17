d965939ab489cd263bfa4a97ad89ed5f84e3593a
徐国方 <xugf@mingdutech.com>
2018-05-17 09:09:20 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+npm install hexo-cli -g --registery=https://registry.npm.taobao.org
+npm install hexo --save --registry=https://registry.npm.taobao.org
+npm install hexo-deployer-git --save --registry=https://registry.npm.taobao.org
+#npm install --registry=https://registry.npm.taobao.org
